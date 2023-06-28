fe7d97a4f5b4c66780ec926309d51617a04bde2a
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-28 16:44:47 +0900

    Initial Commit

---
+++ SampleMVVM/SampleMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleMVVMApp.swift
+//  SampleMVVM
+//
+//  Created by 飯塚政美 on 2023/06/28.
+//
+
+import SwiftUI
+
+@main
+struct SampleMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
